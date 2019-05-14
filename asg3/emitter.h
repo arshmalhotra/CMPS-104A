10c0474db6b2929886b73c9281eb5fabf1ab4cab
Arsh Malhotra <amalhot3@ucsc.edu>
2019-05-13 18:06:58 -0700

    emitter is back

---
+++ asg3/emitter.h
@@ -0,0 +1,15 @@
+// CMPS104A
+// ASSIGNMENT: ASG2
+// NAME 1: Shlok Gharia
+// EMAIL 1: sgharia@ucsc.edu
+// NAME 2: Arsh Malhotra
+// EMAIL 2: amalhot3@ucsc.edu
+
+#ifndef __EMIT_H__
+#define __EMIT_H__
+
+#include "astree.h"
+
+void emit_sm_code (astree*);
+
+#endif
