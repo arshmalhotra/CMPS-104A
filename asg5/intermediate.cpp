5a8c15e36d8da3dea677f1d2081938983daaa9ec
Arsh Malhotra <amalhot3@ucsc.edu>
2019-06-06 22:44:17 -0700

    fixed issues maybe

--- asg5/intermediate.cpp
+++ asg5/intermediate.cpp
@@ -561,7 +561,9 @@ void print_struct(astree* node) {
 
    fprintf(out, "struct s_%s {\n", (node->struct_name).c_str());
 
-   if(structs.find((node->struct_name).c_str()) == structs.end())
+   const char* sname = (node->struct_name).c_str();
+   string* name = *(const_cast<string*>(sname));
+   if(structs.find(name) == structs.end())
       return;
    if(node->children.size() > 1) {
       astree* list = node->children[1];
